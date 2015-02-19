80073f63751ee1a3cc87d3f5e38f88b2b5f3a739
Jayson Potter <iam@jaysonpotter.com>
2015-02-18 20:46:18 -0800

    Just did a little switcharoo on the initialize conditional to get a new collection. Moved it into the render method and made so much more sense for it to be there.

--- src/js/app/views/search/search.js
+++ src/js/app/views/search/search.js
@@ -14,46 +14,39 @@ Marvel.View.Search = Backbone.View.extend({
         this.listenTo(this.collection, 'add', this.renderAdditionalCharacters);
 
         this.name = options.name;
-
-        if (this.collection.length === 0) {
-            this.collection.fetchCharactersSearchResults({name: this.name});
-        }
-
     },
 
-    placeholderMethod: function () {
-        console.log('events from the backbone search view');
-    },
+    placeholderMethod: function () {},
 
     render: function () {
         var searchInput = new Marvel.View.SearchInput();
 
-        this.childViews.push(searchInput);
-
-        this.$el.append(this.template());
+        this.$el.append(this.template()); // technically not a child view, so it doesn't need to be added to the childViews array
         this.$el.append(searchInput.render().el);
 
-        console.log('this collection should be empty upon first time search', this.collection);
+        if (this.collection.length === 0) {
+            this.collection.fetchCharactersSearchResults({name: this.name});
+        } else {
 
-        // if collection exists then render what's there.
-        this.collection.each(function (model) {
-            var characterView = new Marvel.View.Character({model: model});
+            // if collection exists then render what's there.
+            this.collection.each(function (model) {
+                var characterView = new Marvel.View.Character({model: model});
 
-            this.$el.append(characterView.render().el);
+                this.$el.append(characterView.render().el);
 
-            this.childViews.push(characterView);
+                this.childViews.push(characterView);
 
-            console.log('rendered a search result');
+            }, this);
 
-        }, this);
+        }
+
+        this.childViews.push(searchInput);
     },
 
     renderAdditionalCharacters: function (newModel) {
         var characterView = new Marvel.View.Character({model: newModel});
 
         this.$el.append(characterView.render().el);
-
-        console.log('rendered a search result, but should not have');
 
         this.childViews.push(characterView);
     },
